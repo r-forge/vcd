10e81c158185374bbdb038026918cba364b8f393
meyer <unknown@noreply.com>
2002-05-24 12:58:29 +0000

    small bug for visual acuity (female subset)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vcd@68 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/vcd/R/vcd.demo.R
+++ trunk/vcd/R/vcd.demo.R
@@ -233,7 +233,8 @@ if (chapter == 3) {
   ### Visual Acuity ###
   #####################
   data(VisualAcuity)
-  sieveplot(xtabs(Freq ~ right + left, data = VisualAcuity),
+  sieveplot(xtabs(Freq ~ right + left,
+                  data = VisualAcuity, subset = gender == "female"),
             reverse.y = FALSE,
             main = "Unaided distant vision data",
             xlab = "Left Eye Grade",
