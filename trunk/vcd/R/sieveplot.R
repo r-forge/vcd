2e9768908fbd67e894b7afd2d97a41e019308571
meyer <unknown@noreply.com>
2002-05-24 12:43:46 +0000

    major revision of sieveplot(): no longer uses layout() now.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vcd@66 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/vcd/R/sieveplot.R
+++ trunk/vcd/R/sieveplot.R
@@ -34,13 +34,14 @@ function (formula, data = NULL, ..., subset)
            sieve.lty = c("longdash", "solid"),
            exp.color = "gray",
            exp.lty = "dotted",
-           margin = 0.2,
-           cex.main = 3,
-           cex.lab = 2,
+           margin = 0.01,
+           cex.main = 2,
+           cex.lab = 1.5,
            xlab = names(dimnames(x))[2],
            ylab = names(dimnames(x))[1],
            ...)
 {
+  ## parameter handling
   if (length(dim(x)) > 2)
     stop ("Function only implemented for two-way tables")
   
@@ -50,92 +51,101 @@ function (formula, data = NULL, ..., subset)
   if (is.null(main))
       main <- if (type == "sieve") "Sieve diagram" else "Expected frequencies"
 
-  if (reverse.y) x <- x[nrow(x):1,]
+  nc   <- ncol(x)
+  nr   <- nrow(x)
+  if (reverse.y) x <- x[nr:1,]
 
   ## compute relative frequencies
   n <- sum(x)
   cols <- colSums(x)/n
   rows <- rowSums(x)/n
 
-  ## expected values and signs of deviations
+  ## expected values
   ex <- rows %o% cols * n
+
+  ## signs of deviations
   sgn <- ex - x < 0
 
-  par.save <- par(no.readonly = TRUE)
+  ## box coordinates for expected areas
+  x1 <- c(0, cumsum(cols + margin)[-nc])
+  x2 <- x1 + cols
+  xmid <- (x1 + x2) / 2
   
-  ## build layout
-  layout(matrix(1:(ncol(x) * nrow(x)), nrow(x), ncol(x), byrow=TRUE),
-         widths = cols,
-         heights = rows
-         )
+  y2 <- 1 + (nr - 1) * margin - c(0, cumsum(rows + margin)[-nr])
+  y1 <- y2 - rows
+  ymid <- (y1 + y2) / 2
 
-  ## set margins
-  par(oma = c(5, 5, 10, 5),
-      mar = rep(margin,4)
-      )
-
-  ## title, etc.
+  ## setup device
+  par(mar = c(5.1, 5.1, 6.1, 3.1))
   plot.new()
+  par(usr = c(0, 1 + (nc - 1) * margin, 0, 1 + (nr - 1) * margin))
+  
+  ## title, etc.
   title(main,
         xlab = xlab,
         ylab = ylab,
-        outer=TRUE,
         cex.main = cex.main,
         cex.lab = cex.lab,
         ...)
 
   ## boxes
-  for (j in 1:nrow(x))
-    for (i in 1:ncol(x)) {
-      ## write labels
-      if (i == 1) {
-        axis(2, at = 0.5, labels = dimnames(x)[[1]][j], tick = FALSE, outer = TRUE, ...)
+  for (i in 1:nr)
+    for (j in 1:nc) {
+      
+      ## WRITE LABELS
+
+      if (j == 1) { ## y-axis
+        axis(2, at = ymid[i], labels = dimnames(x)[[1]][i], tick = FALSE, ...)
         ## optionally, write marginal frequencies
         if (values %in%  c("margins","both"))
-          axis(4, at = 0.5, font = 2,
-               labels = if (frequencies == "relative") round(rows[j], 2)
-                        else round(rows[j] * n, 1),
-               tick = FALSE, outer = TRUE, ...)
+          axis(4, at = ymid[i], font = 2,
+               labels = if (frequencies == "relative") round(rows[i], 2)
+                        else round(rows[i] * n, 1),
+               tick = FALSE, ...)
       }
-      if (j == 1) {
+      if (i == 1) { ## x-axis
         axis(1 + 2 * values %in% c("margins","both"),
-             at = 0.5, labels = dimnames(x)[[2]][i], tick = FALSE, outer = TRUE, ...)
+             at = xmid[j], labels = dimnames(x)[[2]][j], tick = FALSE, ...)
         ## optionally, write marginal frequencies
         if (values %in%  c("margins","both"))
-          axis(1, at = 0.5, font = 2,
-               labels = if (frequencies == "relative") round(cols[i], 2)
-                        else round(cols[i] * n, 1),
-               tick = FALSE, outer = TRUE, ...)
+          axis(1, at = xmid[j], font = 2,
+               labels = if (frequencies == "relative") round(cols[j], 2)
+                        else round(cols[j] * n, 1),
+               tick = FALSE, ...)
       }
 
-      ## draw grid
-      square.side <- sqrt(cols[i] * rows[j] / if (type=="sieve") x[j, i] else ex[j, i])
-      dev <- sgn[j, i] + 1
-      grid(round(cols[i]/square.side), round(rows[j]/square.side),
-           col = if (type == "sieve") sieve.colors[dev] else exp.color,
-           lty = if (type == "sieve") sieve.lty[dev] else exp.lty
-           )
+      ## DRAW GRID
 
-      ## optionally, write cell frequencies
+      square.side <- sqrt(cols[j] * rows[i] / if (type == "sieve") x[i, j] else ex[i, j])
+      dev <- sgn[i, j] + 1
+      line.color <- if (type == "sieve") sieve.colors[dev] else exp.color
+      line.type  <- if (type == "sieve") sieve.lty[dev] else exp.lty
+      for (ii in seq(0, rows[i], by = square.side))
+        lines(c(x1[j], x2[j]), c(y1[i], y1[i]) + ii,
+              col = line.color, lty = line.type
+              )
+      for (jj in seq(0, cols[j], by = square.side))
+        lines(c(x1[j], x1[j]) + jj, c(y1[i], y2[i]),
+              col = line.color, lty = line.type
+              )
+        
+      ## OPTIONALLY, WRITE CELL FREQURNCIES
+
       if (values %in% c("cells","both"))
-          text(0.5, 0.5,
+          text(xmid[j], ymid[i],
                if (frequencies == "relative")
-                 round((if (type == "sieve") x[j, i] else ex[j, i]) / n, 2)
+                 round((if (type == "sieve") x[i, j] else ex[i, j]) / n, 2)
                else
-                 round((if (type == "sieve") x[j, i] else ex[j, i]), 1),
+                 round((if (type == "sieve") x[i, j] else ex[i, j]), 1),
                font = 2, ...
                )
 
-      ## border
-      box()
+      ## BORDER
 
-      ## new frame
-      if (j < nrow(x) || i < ncol(x)) plot.new()
+      rect(x1[j], y1[i], x2[j], y2[i], ...)
     }
-
-  par(par.save)
-  
 }
+
 
 
 
