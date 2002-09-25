3eef7418e9d5fe4f7d485520b7df257ff9955f23
zeileis <zeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2002-09-25 15:26:05 +0000

    preliminary docs added for mosaicpairs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vcd@108 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/vcd/R/mosaicpairs.R
+++ trunk/vcd/R/mosaicpairs.R
@@ -1,6 +1,6 @@
 "mosaicpairs" <-
   function(x, main = NULL, xlab = NULL, ylab = NULL, labels, ...,
-           type = c("simple", "independend", "conditional"),
+           type = c("simple", "independent", "conditional"),
            shade = FALSE, oma = NULL, cex.labels = NULL, label.pos = 0.5,
 	   font.labels = 1, gap = 1)
   {
@@ -46,7 +46,7 @@
                          {
                            switch(type,
                                   simple = mosaicplot(margin.table(x,c(j,i)),shade=shade,clegend=FALSE, xlab="",ylab="",cex=1 ),
-                                  independend = mosaicplot(x,shade=shade,clegend=FALSE, xlab="",ylab="",cex=1 ),       
+                                  independent = mosaicplot(x,shade=shade,clegend=FALSE, xlab="",ylab="",cex=1 ),
                                   conditional = mosaicplot(margin.table(x,c(j,i,index[!index%in%c(j,i)])), shade=shade,margin=list(c(j,index[!index%in%c(j,i)]),c(i,index[!index%in%c(j,i)])), clegend=FALSE, xlab="",ylab="",cex=1.2 )
                                   )
                            
