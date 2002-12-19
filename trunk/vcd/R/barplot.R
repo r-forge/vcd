f55a56e95eec090d4ebe675a338775f0b97e77cc
zeileis <unknown@noreply.com>
2002-12-19 22:00:01 +0000

    *** empty log message ***
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vcd@164 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/vcd/R/barplot.R
+++ trunk/vcd/R/barplot.R
@@ -7,7 +7,7 @@ function(height, width = 1, space = NULL, names.arg = NULL,
 	 xlim = NULL, ylim = NULL, xpd = TRUE,
 	 axes = TRUE, axisnames = TRUE,
 	 cex.axis = par("cex.axis"), cex.names = par("cex.axis"),
-	 inside = TRUE, plot = TRUE, axis.lty = 0, shift = 0, ...)
+	 inside = TRUE, plot = TRUE, axis.lty = 0, offset = 0, ...)
 {
     if (!missing(inside)) .NotYetUsed("inside", error = FALSE)# -> help(.)
 
@@ -44,18 +44,18 @@ function(height, width = 1, space = NULL, names.arg = NULL,
 	height <- rbind(0, apply(height, 2, cumsum))
     }
 
-    shift <- rep(shift, length.out = length(width))
+    offset <- rep(offset, length.out = length(width))
 
     delta <- width / 2
     w.r <- cumsum(space + width)
     w.m <- w.r - delta
     w.l <- w.m - delta
     if (horiz) {
-	if (missing(xlim)) xlim <- range(-0.01 * height + shift, height + shift, na.rm=TRUE)
+	if (missing(xlim)) xlim <- range(-0.01 * height + offset, height + offset, na.rm=TRUE)
 	if (missing(ylim)) ylim <- c(min(w.l), max(w.r))
     } else {
 	if (missing(xlim)) xlim <- c(min(w.l), max(w.r))
-	if (missing(ylim)) ylim <- range(-0.01 * height + shift, height + shift, na.rm=TRUE)
+	if (missing(ylim)) ylim <- range(-0.01 * height + offset, height + offset, na.rm=TRUE)
     }
     if (beside)
 	w.m <- matrix(w.m, nc = NC)
@@ -74,13 +74,13 @@ function(height, width = 1, space = NULL, names.arg = NULL,
 		rect(y1,x1, y2,x2, ...)
 	}
 	if (beside)
-	    xyrect(0 + shift, w.l, c(height) + shift, w.r, horizontal = horiz,
+	    xyrect(0 + offset, w.l, c(height) + offset, w.r, horizontal = horiz,
 		   angle = angle, density = density, col = col, border = border)
 	else {
 	    ## noInside <- NC > 1 && !inside # outside border, but not inside
 	    ## bordr <- if(noInside) 0 else border
 	    for (i in 1:NC) {
-		xyrect(height[1:NR, i] + shift[i], w.l[i], height[-1, i] + shift[i], w.r[i],
+		xyrect(height[1:NR, i] + offset[i], w.l[i], height[-1, i] + offset[i], w.r[i],
 		       horizontal = horiz, angle = angle, density = density,
 		       col = col, border = border)# = bordr
                 ## if(noInside)
