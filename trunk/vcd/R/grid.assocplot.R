6182723fa2a29821e22b2e78fbd7f89c8448eb10
zeileis <zeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2003-01-31 14:48:01 +0000

    *** empty log message ***
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vcd@186 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/vcd/R/grid.assocplot.R
+++ trunk/vcd/R/grid.assocplot.R
@@ -1,16 +1,32 @@
-grid.assocplot <- function(x, scale = 0.4, margin = 5)
+grid.assocplot <- function(x, xlab = NULL, ylab = NULL, main = NULL,
+                           model.formula = NULL, transpose = TRUE, labels = TRUE,
+			   color.type = "Friendly", legend = TRUE,
+                           scale = 0.4, margin = 5, rot = 90, check.overlap = TRUE)
 {
-  xlab <- names(dimnames(x))[2]
-  ylab <- names(dimnames(x))[1]
+  require(MASS)
 
-  expctd <- expected(x)
+  if(is.null(xlab)) xlab <- names(dimnames(x))[1]
+  if(is.null(ylab)) ylab <- names(dimnames(x))[2]
+
+  if(is.null(model.formula))
+    model.formula <- as.formula(paste("~ ", paste(names(dimnames(x)), collapse = " + "), sep = " "))
+
+  x.fit <- loglm(model.formula, data = x, fit = TRUE)
+  expctd <- as.matrix(fitted(x.fit)) ## Should transposing really be done??
   x <- as.matrix(x)
   sexpctd <- sqrt(expctd)
-  res <- (x - expctd)/sexpctd
+  res <- as.matrix(residuals(x.fit, type = "pearson"))
 
-  cols <- res
-  cols[cols>0] <- "black" ## or better color schemes
-  cols[cols<=0] <- "red"
+  ## Transposing - why??
+  if(transpose)
+  {
+    expctd <- t(expctd)
+    x <- t(x)
+    sexpctd <- t(sexpctd)
+    res <- t(res)
+  }
+
+  cols <- colorscheme(res, type = color.type)
 
   x.width <- apply(sexpctd, 2, max)
   y.min <- apply(res, 1, min)
@@ -38,18 +54,29 @@ grid.assocplot <- function(x, scale = 0.4, margin = 5)
     grid.move.to(-x.delta/2, 0, default.units = "native")
     grid.line.to(x.width[cl] + x.delta/2, 0,
                  default.units = "native", gp = gpar(lty = "dashed"))
-    if(cl == 1) grid.text(names(y.height)[rw], y = unit(0, "native"),
-                          x = unit(-x.delta/2, "native") + unit(-1.2, "lines"), rot = 90)
-    if(rw == nrows) grid.text(names(x.width)[cl], x = unit(0.5, "npc"),
-                          y = unit(y.min[rw] - y.delta/2, "native") + unit(-1.2, "lines"))
+    if(labels) {
+      if(cl == 1) grid.text(names(y.height)[rw], y = unit(0, "native"),
+        x = unit(-x.delta/2, "native") + unit(-1.2, "lines"), rot = rot,
+	check.overlap = check.overlap)
+      if(rw == nrows) grid.text(names(x.width)[cl], x = unit(0.5, "npc"),
+        y = unit(y.min[rw] - y.delta/2, "native") + unit(-1.2, "lines"),
+	check.overlap = check.overlap)
+    }
     ## grid.rect(gp = gpar(lty = "dashed", col = "light grey"))
     pop.viewport()
   }
 
   grid.newpage()
+
+  if(legend) {
+    push.viewport(viewport(layout = grid.layout(1, 2,
+                  widths = unit(c(0.85, 0.15), "npc"))))
+    push.viewport(viewport(layout.pos.row = 1, layout.pos.col = 1))
+  }
+
   push.viewport(viewport(x = unit(margin, "lines"),
                 y = unit(margin, "lines"),
-  	        width = unit(1, "npc") - unit(2*margin, "lines"),
+  	        width = unit(1, "npc") - unit(1.5*margin, "lines"),
   	        height = unit(1, "npc") - unit(2*margin, "lines"),
                 xscale = c(0, sum(x.width + x.delta)),
                 yscale = c(0, sum(y.height + y.delta)),
@@ -64,9 +91,63 @@ grid.assocplot <- function(x, scale = 0.4, margin = 5)
     }
   }
   ## grid.rect(gp = gpar(lty = "dashed", col = "light grey"))
-
   pop.viewport()
-  grid.text(names(dimnames(x))[2], y = unit(1, "lines"))
-  grid.text(ylab, x = unit(1, "lines"), rot = 90)
+
+  grid.text(xlab, y = unit(1, "lines"), check.overlap = check.overlap)
+  grid.text(ylab, x = unit(1, "lines"), rot = rot, check.overlap = check.overlap)
+  grid.text(main, y = unit(1, "npc") - unit(2, "lines"), gp = gpar(fontsize = 20))
+
+  if(legend) {
+    pop.viewport()
+    push.viewport(viewport(layout.pos.row = 1, layout.pos.col = 2))
+    res.range <- range(res)
+    push.viewport(viewport(x = 0.1, y = 0.2, just = c("left", "bottom"),
+                  yscale = range(res), default.unit = "npc",
+                  height = 0.6, width = 0.6))
+    colbins <- 100
+    for(i in 1:colbins) {
+      yval <- res.range[1] + diff(res.range) * ((i-1)/colbins)
+      ycol <- colorscheme(yval, type = color.type)
+      grid.rect(y = unit(yval, "native"), height = unit(1/colbins, "npc"),
+                gp = gpar(fill = ycol, col = ycol),
+		just = c("centre", "bottom"))
+    }
+    grid.rect()
+    grid.yaxis()
+    grid.text("Pearson\nresiduals:", x = 0.1, y = unit(1, "npc") + unit(0.5, "lines"),
+              gp = gpar(fontsize = 10), just = c("left", "bottom"))
+    pop.viewport()
+    pop.viewport()
+  }
+
+  invisible(x)
 }
 
+colorscheme <- function(residuals, type = c("Friendly", "Shading", "CI"))
+{
+    type <- match.arg(type)
+    switch(type,
+
+    "Friendly" = {
+      color <- residuals
+      color[color > 0] <- "black"
+      color[color <= 0] <- "red"
+    },
+
+    "Shading" = {
+      colorvec <- c(hsv(0, s = seq(1, to = 0, length = 3)),
+                    hsv(2/3, s = seq(0, to = 1, length = 3)))
+      color <- residuals
+      color[residuals > 4] <- colorvec[6]
+      color[residuals <= -4] <- colorvec[1]
+      color[residuals > -4 & residuals <= -2] <- colorvec[2]
+      color[residuals > -2 & residuals <= 2] <- colorvec[3]
+      color[residuals > 2 & residuals <= 4] <- colorvec[5]
+    },
+
+    "CI" = {
+      stop("sorry, not yet implemented")
+    })
+
+    return(color)
+}
