466862a677d647266c19f1e44601e81417d59600
zeileis <unknown@noreply.com>
2003-02-10 17:49:50 +0000

    *** empty log message ***
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vcd@200 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/vcd/R/assocplot.R
+++ trunk/vcd/R/assocplot.R
@@ -179,16 +179,18 @@ pearson.test <- function (x, n = 1000, FUN = function(x) max(abs(x)),
   FUNPearson <- function(x) FUN(Pearson(x))
   dist <- sapply(r2dtable(n, rowTotals, colTotals), FUNPearson)
   STATISTIC <- FUNPearson(x)
+  pdist <- function(x) mean(dist <= x)
+  qdist <- function(p) quantile(dist, p)
   PVAL <- switch(alternative,
-                 greater = sum(dist >= STATISTIC) / n,
-                 less = sum(dist <= STATISTIC) / n
+                 greater = mean(dist >= STATISTIC),
+                 less = mean(dist <= STATISTIC)
                  )
-  CINT <- switch(alternative,
-                 greater = c(0, quantile(dist, conf.level)),
-                 less = c(quantile(dist, 1 - conf.level), 0),
-                 )
-  names(CINT) <- NULL
-  attr(CINT, "conf.level") <- conf.level
+  ## CINT <- switch(alternative,
+  ##                greater = c(0, quantile(dist, conf.level)),
+  ##                less = c(quantile(dist, 1 - conf.level), 0),
+  ##                )
+  ## names(CINT) <- NULL
+  ## attr(CINT, "conf.level") <- conf.level
   METHOD <- "Permutation test of independence (based on simulated Pearson residuals)"
   names(STATISTIC) <- "P"
   structure(list(statistic = STATISTIC,
@@ -198,8 +200,10 @@ pearson.test <- function (x, n = 1000, FUN = function(x) max(abs(x)),
                  observed = x,
                  expected = expected,
                  residuals = Pearson(x),
-                 conf.int = CINT,
-                 dist = if (return.distribution) dist else NULL),
+                 dist = if (return.distribution) dist else NULL,
+                 ## conf.int = CINT,
+		 qdist = qdist,
+		 pdist = pdist),
             class = "htest"
             )
 }
