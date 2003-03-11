5c3484a1260e2df9605bc1fc2c0e6db3abee16b2
meyer <unknown@noreply.com>
2003-03-11 20:42:39 +0000

    cosmetics
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vcd@224 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/vcd/R/hls2rgb.R
+++ trunk/vcd/R/hls2rgb.R
@@ -1,15 +1,15 @@
 hls <- function(H, L, S) {
   
-  RGB <- function (q1, q2, hue) {
+  RGB <- function(q1, q2, hue) {
     if (hue > 360) hue <- hue - 360
     if (hue < 0) hue <- hue + 360
     if (hue < 60) 
       q1 + (q2 - q1) * hue / 60
-        else if (hue < 180)
-          q2
-        else if (hue < 240) 
-          q1 + (q2 - q1) * (240 - hue) / 60
-        else q1
+    else if (hue < 180)
+      q2
+    else if (hue < 240) 
+      q1 + (q2 - q1) * (240 - hue) / 60
+    else q1
   }
 
   H <- H * 360
