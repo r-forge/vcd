61dfb7108572ed1d8da19323008df92af5aa3c24
zeileis <zeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2004-03-01 19:44:26 +0000

    now load MASS and grid automatically
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vcd@291 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/vcd/R/zzz.R
@@ -0,0 +1,2 @@
+if(!("package:MASS" %in% search() || require(MASS))) warning("could not load package MASS")
+if(!("package:grid" %in% search() || require(grid))) warning("could not load package grid")
