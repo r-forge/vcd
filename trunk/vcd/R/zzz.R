4bf1890f0b2b5876e0a52ae43595a9d99b1694ba
zeileis <unknown@noreply.com>
2004-03-04 11:24:07 +0000

    *** empty log message ***
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vcd@292 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/vcd/R/zzz.R
+++ trunk/vcd/R/zzz.R
@@ -1,2 +1,12 @@
 if(!("package:MASS" %in% search() || require(MASS))) warning("could not load package MASS")
 if(!("package:grid" %in% search() || require(grid))) warning("could not load package grid")
+
+if(!exists("pushViewport")) {
+  if(!exists("push.viewport")) stop("pushViewport/push.viewport cannot be found")
+  pushViewport <- push.viewport
+}
+
+if(!exists("popViewport")) {
+  if(!exists("pop.viewport")) stop("popViewport/pop.viewport cannot be found")
+  popViewport <- pop.viewport
+}
