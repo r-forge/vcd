82e17a37a106b40287bc1cfd7704be3a71cc7feb
zeileis <zeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2004-12-23 16:17:21 +0000

    *** empty log message ***
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vcd@313 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/vcd/R/aaa.R
+++ trunk/vcd/R/aaa.R
@@ -1,3 +1,6 @@
-.onLoad <- function(lib, pkg)
-  if(!require(grid))
-    stop("Can't load package grid.")
+.onLoad <- function(lib, pkg) {
+  if(!("package:grid" %in% search() || require(grid))) 
+    stop("could not load package grid")
+  if(!("package:MASS" %in% search() || require(MASS)))
+    stop("could not load package MASS")
+}
