9ae93f02fe59172499757ec6ad327e6a36e27373
zeileis <zeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-01-03 13:00:43 +0000

    plot method for fitted loglinear models (can call mosaic or assoc)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vcd@315 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/vcd/R/plot.loglm.R
@@ -0,0 +1,29 @@
+plot.loglm <- function(x,
+                       panel = mosaic,
+                       type = c("observed", "expected"),
+                       residuals.type = c("pearson", "deviance"),
+                       ...)
+{
+  residuals.type <- match.arg(residuals.type)
+  if(is.null(x$fitted)) x <- update(x, fitted = TRUE)
+  expected <- fitted(x)
+  residuals <- residuals(x, type = "pearson")
+  observed <- residuals * sqrt(expected) + expected
+  if(residuals.type == "deviance") residuals <- residuals(x, type = "deviance")
+  panel(observed, residuals = residuals, expected = expected, df = x$df,
+        type = type, shade = TRUE, ...)
+}
+
+#Z# example:
+#Z# 
+#Z# data(PreSex)
+#Z# tab <- xtabs(Freq ~ PremaritalSex + ExtramaritalSex + Gender + MaritalStatus,
+#Z#   data = as.data.frame(PreSex))
+#Z# mosaic(tab)
+#Z# fm <- loglm(~ PremaritalSex * ExtramaritalSex * (Gender + MaritalStatus),
+#Z#   data = tab)
+#Z# fm
+#Z# plot(fm)
+#Z# plot(fm, split = TRUE)
+#Z# plot(fm, split = TRUE, type = "expected")
+#Z# plot(fm, residuals = "deviance")
