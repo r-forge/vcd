ae411c5a022714d5434d08bff401e72e9bbb3ccc
dmeyer (David Meyer) <David.Meyer@R-project.org>
2014-12-01 14:26:53 +0000

    add layout calculation and main / subtitle handling to splot
    - This line, and those below, will be ignored--
    
    M    R/strucplot.R
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vcd@760 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/vcd/R/strucplot.R
+++ pkg/vcd/R/strucplot.R
@@ -243,23 +243,71 @@ strucplot <- function(## main parameters
                       split_vertical = split_vertical), .FUN = FUN))
 }
 
-splot <- function(..., layout = NULL, main = NULL, gp_main = gpar(),
-                  sub = NULL, gp_sub = gpar()) {
+splot <- function(...,
+                  layout = NULL,
+                  main = NULL, gp_main = gpar(fontsize = 20),
+                  sub = NULL, gp_sub = gpar(fontsize = 15),
+                  keep_aspect_ratio = TRUE) {
     l <- list(...)
+    ll <- length(l)
+    m <- !is.null(main)
+    s <- !is.null(sub)
+
+    ## calculate layout
     if (is.null(layout))
-        layout <- c(1, length(l))
+        layout <- c(trunc(sqrt(ll)), ceiling(ll / trunc(sqrt(ll))))
+
+    ## push base layout
     grid.newpage()
+    hts = unit(1 - 0.1 * m - 0.1 * s, "null")
+    if (m)
+        hts <- c(unit(0.1, "null"), hts)
+    if (s)
+        hts <- c(hts, unit(0.1, "null"))
+    pushViewport(viewport(layout =
+                              grid.layout(nrow = 1 + m + s,
+                                          ncol = 1, heights = hts)
+                          )
+                 )
+
+    ## push main, if any
+    if (!is.null(main)) {
+        pushViewport(viewport(layout.pos.row = 1,
+                              layout.pos.col = NULL))
+
+        grid.text(main, gp = gp_main)
+        popViewport(1)
+    }
+
+    ## push strucplots
+    pushViewport(viewport(layout.pos.row = 1 + m,
+                          layout.pos.col = NULL))
     pushViewport(viewport(layout = grid.layout(nrow = layout[1],
-                          ncol = layout[2])))
+                          ncol = layout[2])
+                          )
+                 )
     count <- 1
     for (i in seq_len(layout[1]))
         for (j in seq_len(layout[2])) {
             pushViewport(viewport(layout.pos.row = i,
                                   layout.pos.col = j))
+            pushViewport(viewport(width = 1,
+                                  height = 1,
+                                  default.units = if (keep_aspect_ratio) "snpc" else "npc"))
             attr(l[[count]], ".FUN")(FALSE)
-            popViewport()
+            popViewport(2)
             count <- count + 1
         }
+    popViewport(2)
+
+    ## push sub, if any
+    if (!is.null(sub)) {
+        pushViewport(viewport(layout.pos.row = 1 + m + s,
+                              layout.pos.col = NULL))
+        grid.text(sub, gp = gp_sub)
+        popViewport()
+    }
+
     popViewport(1)
 }
 
